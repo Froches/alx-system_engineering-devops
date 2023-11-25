75f37dfd77d956136e51e144dcd2864760e2cd15
Froches <michaelfavour418@gmail.com>
2023-11-25 02:00:29 -0800

    installing a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,11 +2,11 @@
 # Install a specific version of flask (2.1.0)
 
 package { 'flask':
-  ensure    => '2.1.0',
-  provider  => 'pip3'
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
 
 package { 'werzeug':
-  ensure    => '2.1.1',
-  provider  => 'pip3'
+  ensure   => '2.1.1',
+  provider => 'pip3'
 }
