d42638f8afadeba20ad1e751821b2f13f708cfc6
Froches <michaelfavour418@gmail.com>
2023-11-25 01:50:34 -0800

    installing a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,6 +2,6 @@
 # Install a specific version of flask (2.1.0)
 
 exec { 'install_flask':
- command => 'pip3 install Flask==2.1.0',
- path   => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
+  command => 'pip3 install Flask==2.1.0',
+  path    => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
 }
