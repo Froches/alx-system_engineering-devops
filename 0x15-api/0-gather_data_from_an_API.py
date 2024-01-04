19b93fd4c09b90d63c4b98d2784b9f04ac52dc6f
Froches <michaelfavour418@gmail.com>
2024-01-04 13:56:34 -0800

    gather data from API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,34 +1,30 @@
 #!/usr/bin/python3
 """
-Module to gather employee data
+Gathering data from an API
 """
+import re
 import requests
 import sys
 
 
-def get_employee_todo_progress(employee_id):
-    # Base URL
-    url = "https://jsonplaceholder.typicode.com/"
-
-    # Fetch user data
-    user = requests.get(url + f"users/{employee_id}").json()
-
-    # Fetch todos for the user
-    todos = requests.get(url + "todos", params={"userId": employee_id}).json()
-
-    # Calculate progress
-    completed_tasks = [task for task in todos if task.get('completed') is True]
-    total_tasks = len(todos)
-
-    print(
-        f"Employee {user.get('name')} is done with tasks("
-        f"{len(completed_tasks)}/{total_tasks}):"
-    )
-    for task in completed_tasks:
-        print("\t {}".format(task.get('title')))
+API = "https://jsonplaceholder.typicode.com"
 
 
-# Usage: python script.py [EMPLOYEE_ID]
-if __name__ == "__main__":
-    employee_id = sys.argv[1]
-    get_employee_todo_progress(employee_id)
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            id = int(sys.argv[1])
+            user_res = requests.get('{}/users/{}'.format(API, id)).json()
+            todos_res = requests.get('{}/todos'.format(API)).json()
+            user_name = user_res.get('name')
+            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
+            todos_done = list(filter(lambda x: x.get('completed'), todos))
+            print(
+                'Employee {} is done with tasks({}/{}):'.format(
+                    user_name,
+                    len(todos_done),
+                    len(todos)
+                )
+            )
+            for todo_done in todos_done:
+                print('\t {}'.format(todo_done.get('title')))
